56f6cec0b1b49b1fde4d325db47437db68cd765d
Pjoshi2005 <147191983+Pjoshi2005@users.noreply.github.com>
2025-11-14 15:15:13 +0530

    traverse LL

---
+++ TraverseLinkedlist.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> LLTraversal(ListNode *head) {
+        //assign head to teamp 
+        ListNode* temp = head;
+        vector<int> ans;
+        //traverse temp until u cant find null pointer and push vaue to ans and move temp to next
+        while(temp!= nullptr){
+            ans.push_back(temp->val);
+            temp =temp->next;
+        }
+        return ans;
+
+
+    }
+};
+
