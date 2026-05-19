c8536d7ca1972e2ea1730b446d5a12a1e30e96d5
Pjoshi2005 <147191983+Pjoshi2005@users.noreply.github.com>
2026-05-19 23:53:42 +0530

    Union of two sorted array

---
+++ Array/2 pointer/unionOf2SortArray.cpp
@@ -0,0 +1,40 @@
+class Solution {
+   public:
+    vector<int> unionArray(vector<int>& nums1, vector<int>& nums2) {
+        int i = 0;
+        int j = 0;
+
+        vector<int> nums;
+
+        while (i < nums1.size() && j < nums2.size()) {
+            if (nums1[i] <= nums2[j]) {
+                if (nums.empty() || nums.back() != nums1[i]) {
+                    nums.push_back(nums1[i]);  
+                }
+                i++;
+            } else {
+                if (nums.empty() || nums.back() != nums2[j]) {
+                    nums.push_back(nums2[j]);
+                }
+                j++;
+            }
+        }
+
+        while ( i < nums1.size()) {
+            if(nums.back() != nums1[i]){
+            nums.push_back(nums1[i]);
+            
+            }
+            i++;
+        }
+        while ( j < nums2.size()) {
+            if(nums.back() != nums2[j]){
+            nums.push_back(nums2[j]);
+            
+            }
+            j++;
+        }
+
+        return nums;
+    }
+};
